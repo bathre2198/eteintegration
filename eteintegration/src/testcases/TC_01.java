4d7d802713f43a3176c5696f7d768d9249307fc1
HOME <HOME@LAPTOP-LMM7U3RG>
2020-12-29 12:16:08 +0530

    create automation project

---
+++ eteintegration/src/testcases/TC_01.java
@@ -0,0 +1,31 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_01 {
+
+	@Test
+	public void testcase01() throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		Thread.sleep(3000);
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		driver.findElementByXPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input").sendKeys("google");
+		Thread.sleep(3000);
+		driver.quit();
+		
+		
+		
+		
+		
+	}
+	
+		
+
+
+	
+
+}
